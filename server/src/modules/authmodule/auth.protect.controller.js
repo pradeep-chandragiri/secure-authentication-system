bf9d3d49850ab6e427ca56578db3079c126086ef
Pradeep C <pradeepchandragiri@outlook.com>
2026-05-12 11:23:15 +0530

    [feature][auth] Add permanent account deletion with complete user data cleanup

--- server/src/modules/authmodule/auth.protect.controller.js
+++ server/src/modules/authmodule/auth.protect.controller.js
@@ -51,7 +51,39 @@ export const logout = async (req, res) => {
 export const delete_account = async (req, res) => {
     
     try {
+
+        const userId = req.user.userId
+        const token = req.cookies.token
+
+        if (!token){
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid token.'
+            })
+        }
+
+        // check if token exists in the blacklist
+        const [existing_token] = await db.query(`SELECT token FROM blacklisted_tokens WHERE token = ?`, [token])
+
+        if(existing_token.length > 0){
+            return res.status(401).json({
+                success: false,
+                message: 'Session Expired.'
+            })
+        }
+
+        await db.query(`INSERT INTO user_activity_logs (userId, action_type, description, ip_address, user_agent) VALUES (?, ?, ?, ?, ?)`, [userId, 'ACCOUNT_DELETION', 'Accound deleted successfully', req.ip || 'Unknown', req.headers['user-agent'] || 'Unknown'])
+
+        await db.query(`DELETE FROM users WHERE userId = ?`, [userId])
+        await db.query(`DELETE FROM user_activity_logs WHERE userId = ?`, [userId])
         
+        res.clearCookie('token')
+
+        return res.status(200).json({
+            success: true,
+            message: 'Account deleted successfully'
+        })
+
     } catch (error) {
         return res.status(500).json({
             success: false,
